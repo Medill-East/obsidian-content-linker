77ea86679a1f763bfd9998c73bafa1d75a2d1221
Medill-East <haodong.liao@gmail.com>
2023-08-20 16:16:22 +0800

    version archive 202308201616
    current version could
    1 search the content of the valut
    2 show the list of duplicated content
    3 if you click one of the given options, you will enter the content as bilink format at the place of focus

--- main.ts
+++ main.ts
@@ -1,134 +1,122 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, WorkspaceLeaf, Plugin, TFile, PluginSettingTab, Setting } from 'obsidian';
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ContentLinkerPlugin extends Plugin {
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.addCommand({
+      id: 'search-content',
+      name: 'Search Content',
+      callback: async () => {
+        await this.searchContent();
+      }
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.addSettingTab(new ContentLinkerSettingTab(this.app, this));
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  async searchContent() {
+    const vault = this.app.vault;
+    const files = vault.getMarkdownFiles();
+    const duplicateContent = new Map();
+  
+    for (const file of files) {
+      const content = await vault.cachedRead(file);
+      const regex = /(?<=\[\[).+?(?=\]\])/g;
+      const matches = content.match(regex);
+  
+      if (matches !== null) {
+        for (const match of matches) {
+          if (duplicateContent.has(match)) {
+            duplicateContent.get(match).push(file.path);
+          } else {
+            duplicateContent.set(match, [file.path]);
+          }
+        }
+      }
+    }
+  
+    const options = Array.from(duplicateContent.keys()).map((key) => {
+      const origins = duplicateContent.get(key).join(', ');
+      return {
+        content: key,
+        value: key,
+        description: `Add bi-directional link to: ${key}\nOrigins: ${origins}`
+      };
+    });
+  
+    const modal = new Modal(this.app);
+    modal.open();
+    modal.titleEl.setText('Select an option');
+  
+    modal.contentEl.empty();
+    const listContainer = createEl('ul');
+    modal.contentEl.appendChild(listContainer);
+  
+    for (const option of options) {
+      const listItem = createEl('li');
+      listContainer.appendChild(listItem);
+  
+      const button = createEl('button');
+      button.setText(`[${option.content}]`);
+      button.addEventListener('click', () => {
+        const activeLeaf = this.app.workspace.activeLeaf;
+        if (activeLeaf) {
+          const activeView = activeLeaf.view;
+          const newLink = `[[${option.content}]]`;
+          if (activeView instanceof MarkdownView) {
+            const editor = activeView.editor;
+            editor?.replaceSelection(newLink);
+            editor?.focus();
+          }
+        }
+        modal.close();
+      });
+  
+      listItem.appendChild(button);
+    }
+  }
+  
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ContentLinkerSettingTab extends PluginSettingTab {
+  plugin: ContentLinkerPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: ContentLinkerPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.mySetting)
+        .onChange(async (value) => {
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
